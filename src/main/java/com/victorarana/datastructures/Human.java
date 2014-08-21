c60de06098043e38a9321ece8d652028ac8aa81b
Victor José Arana Rodríguez <victor_arana@hotmail.com>
2014-08-21 09:44:26 -0500

    Default constructor overrided

--- src/main/java/com/victorarana/datastructures/Human.java
+++ src/main/java/com/victorarana/datastructures/Human.java
@@ -5,6 +5,16 @@ public class Human {
 	public int age;			// The Human's age
 	public String name;		// The Human's name
 	
+	public Human(String givenName) {
+		age = 12;
+		name = givenName;
+	}
+	
+	public Human(){
+		age = 0;
+		name = "Untitled";
+	}
+	
 	public void introduce(){
 		System.out.println("I'm " + name + " and I'm " + age + " years old.");
 	}
@@ -12,12 +22,6 @@ public class Human {
 	public void copy(Human original){
 		age = original.age;
 		name = original.name;
-	}
-
-	public Human(String givenName) {
-		age = 12;
-		name = givenName;
-	}
-	
+	}	
 	
 }
