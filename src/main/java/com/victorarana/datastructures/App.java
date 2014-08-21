39318f2be9a36af656eba41165c5384d83422b60
Victor José Arana Rodríguez <victor_arana@hotmail.com>
2014-08-21 09:34:40 -0500

    Lecture03

---
+++ src/main/java/com/victorarana/datastructures/App.java
@@ -0,0 +1,12 @@
+package com.victorarana.datastructures;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Human kayla = new Human();
+        kayla.age = 12;
+        kayla.name = "Kayla";
+        kayla.introduce();
+    }
+}
---
+++ src/main/java/com/victorarana/datastructures/Human.java
@@ -0,0 +1,12 @@
+package com.victorarana.datastructures;
+
+public class Human {
+
+	public int age;			// The Human's age
+	public String name;		// The Human's name
+	
+	public void introduce(){
+		System.out.println("I'm " + name + " and I'm " + age + " years old.");
+	}
+	
+}
